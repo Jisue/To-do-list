7082fd3d9a0b8fd066203408e495e5f3b3ef10a4
Jisue <49144667+Jisue@users.noreply.github.com>
2021-02-03 14:42:22 +0900

    undifined문제

--- src/app.ts
+++ src/app.ts
@@ -5,16 +5,19 @@ import path from 'path'
 import {indexRoutes} from './routes/index';
 import {listRoutes} from './routes/list';
 import dotenv from 'dotenv';
+import bodyParser from 'body-parser';
 
 
 class App{
     public app : express.Application;
-    public indexroute : indexRoutes = new indexRoutes();
+    //public indexroute : indexRoutes = new indexRoutes();
     public listroute : listRoutes = new listRoutes();
 
     constructor(){
         this.app = express();
-        this.indexroute.routes(this.app);
+        this.app.use(bodyParser.json());
+        this.app.use(bodyParser.urlencoded( {extended : false } ));
+        // this.app.use(bodyParser.text());
         this.listroute.routes(this.app);
     }
 }
@@ -30,7 +33,14 @@ app.set('view engine', 'pug')
 app.set('port', process.env.PORT || 3000)
 
 app.use(express.static(path.join(__dirname, 'public')));
-app.use('/', app.route);
+//app.use('/', app.route);
+//app.use(express.json());
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({ extended: true }));
+
+// const listroute : listRoutes = new listRoutes();
+
+// listroute.routes(app);
 
 app.listen(app.get('port'),()=>{
   console.log(app.get('port'),"번 포트에서 대기중입니다.");
--- src/routes/list.ts
+++ src/routes/list.ts
@@ -1,16 +1,37 @@
-import {Request, Response, NextFunction} from 'express';
+import bodyParser from 'body-parser';
+import express, {Request, Response} from 'express';
 import {DB} from '../config/db';
 
 
 const connection = new DB().connection;
 
-export class listRoutes {       
+export class listRoutes {     
     public routes(router:any): void {          
-        router.route('/list').get((request: Request, response: Response,next: NextFunction) => {     
+        router.route('/').get((req: Request, res: Response) => {     
             
             connection.query("SELECT * FROM user_list", function (err, result, fields) {
                 if (err) throw err;
-                response.render('list',{
+                res.render('list',{
+                    list : result
+                });                
+            });
+        })
+
+        router.route('/').post((req: Request, res: Response) => {
+
+            // connection.query(
+            //     "INSERT INTO user_list(list_name, list_dday, list_memo) VALUES('${request.body.list_name}', '${request.body.list_date}','${request.body.list_memo}');", function (err, result, fields) {
+            //     if (err) throw err;              
+            // });
+            console.log("입력")
+            console.log(req.body.list_name);
+            console.log(req.body.list_date);
+            console.log(req.body.list_memo);
+
+            connection.query("SELECT * FROM user_list", function (err, result, fields) {
+                if (err) throw err;
+                console.log(result);
+                res.render('list',{
                     list : result
                 });                
             });
