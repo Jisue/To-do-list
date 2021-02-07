23c41b3c0d50b6076646dd8c89b41cfc88ff6b15
Jisue <49144667+Jisue@users.noreply.github.com>
2021-02-07 17:07:10 +0900

    add rollback

--- src/routes/trash.ts
+++ src/routes/trash.ts
@@ -8,28 +8,53 @@ const connection = new DB().connection;
 export class trashRoutes {   
 
     public routes(router:any): void {          
-        router.route('/delete').post((req: Request, res: Response) => {
-
-            let newDate:Date = new Date();
-            let time:String = newDate.toJSON().slice(0,10);
+        router.route('/trash').get((req: Request, res: Response) => {
+            
+            connection.query("SELECT * FROM user_list WHERE list_on = 0", function (err, result) {
+                if (err) throw err;
+                res.render('trash',{
+                    list : result
+                });        
+            });
+        })
+        router.route('/trash').post((req: Request, res: Response) => {
 
             let delete_index:JSON = req.body.delete_index;
             console.log(delete_index);
+
+            if(delete_index !== undefined)
+            {
+                connection.query(`
+                    DELETE FROM user_list
+                    WHERE list_index = '${delete_index}';`
+                , function (err, result, fields) {
+                    if (err) throw err;              
+                });
+                console.log("영구삭제 되었습니다.");
+
+            }
+
+            let rollback_index:JSON = req.body.rollback_index;
+            console.log(rollback_index);
             
-            connection.query(`
-                UPDATE user_list
-                SET list_on = 0
-                WHERE list_index = '${delete_index}';`
-            , function (err, result, fields) {
-                if (err) throw err;              
-            });
-            console.log("목록 삭제됨");
+            if(rollback_index !== undefined)
+            {
+                connection.query(`
+                    UPDATE user_list
+                    SET list_on = 1
+                    WHERE list_index = '${rollback_index}';`
+                , function (err, result, fields) {
+                    if (err) throw err;              
+                });
+                console.log("복원 되었습니다.");
+
+            }
             
             connection.query("SELECT * FROM user_list", function (err, result) {
                 if (err) throw err;
-                res.render('list',{
-                    list : result,
-                    time : time
+                //console.log(result);
+                res.render('trash',{
+                    list : result
                 });        
             });
         })
